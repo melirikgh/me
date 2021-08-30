e44077c0b7d0ea0954ddb9feb08ec03a143c9543
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-08-31 00:26:52 +0700

    Create post_music0.js

---
+++ css/post_music0.js
@@ -0,0 +1,20 @@
+/* Sumber Lirik */
+#underlirik{margin:20px 0 50px}
+#msource{display:flex;justify-content:center;line-height:50px}
+.msource_text{margin-right:5px}
+#msource a{display:block;cursor:none}
+#msource img{margin:0 0 2.25px;pointer-events:none}
+/* Listening */
+#listening{text-align:center}
+.yt_text{font-size:20px;padding:10px 0 10px;border-left:5px solid rgba(0,0,0,0.05);border-right:5px solid rgba(0,0,0,0.05);border-top:5px solid rgba(0,0,0,0.05);border-radius:10px 10px 0 0}
+#yt_link{background-image:url("data:image/svg+xml,%3Csvg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z' fill='%234285F4'/%3E%3C/svg%3E");width:20px;height:20px;margin:0 5px;vertical-align:-2px}
+.yt_box{position:relative;width:100%;padding-bottom:56.25%;height:0;overflow:hidden;background:#0F9D58}
+.yt_box iframe{position:absolute;top:0;left:0;width:100%;height:100%;border:0}
+.dengarkan{display:flex;justify-content:center;padding:5px 0 5px;border-left:5px solid rgba(0,0,0,0.05);border-right:5px solid rgba(0,0,0,0.05);border-bottom:5px solid rgba(0,0,0,0.05);border-radius:0px 0px 10px 10px}
+.listen_text{line-height:60px;margin-right:5px}
+.listen_box{cursor:pointer;width:60px;height:60px}
+.listen_box a{display:block;color:rgba(0,0,0,.80)}
+.listen_box a:hover{background:#191414;color:#fff}
+.listen_box img{margin-top:3.5px;width:30px;height:30px;pointer-events:none}
+/* DISCLAIMER */
+.m_infokey{background:#ffeaa7;font-size:15px;padding:25px;margin:50px 0 0;border:3px solid rgba(0,0,0,0.05);border-radius:10px}
