7891a5c1a795e038992fe7258c1ddf1f66934f86
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-08-31 00:33:40 +0700

    Create single0.js

---
+++ js/single0.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){$("#flippy").click(function(){$("#flippanel").slideToggle("normal")})});
+// Double Click
+$('i[rel="pre"]').replaceWith(function(){return $("<pre><code>"+$(this).html()+"</code></pre>")});for(var pres=document.querySelectorAll("pre,code,kbd,blockquote,td"),i=0;i<pres.length;i++)pres[i].addEventListener("dblclick",function(){var e=getSelection(),t=document.createRange();t.selectNodeContents(this),e.removeAllRanges(),e.addRange(t)},!1);
