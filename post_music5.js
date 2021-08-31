5d831819a12ab6d317d201bd66d8f3bd8f236443
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-08-31 21:55:45 +0700

    Update and rename post_music4.js to post_music5.js

--- post_music5.js
+++ post_music5.js
@@ -1,2 +1,2 @@
 loadyn("https://cdn.statically.io/gh/melirikgh/me/main/css/post_music0.min.css","css")
-loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/post_music4.min.js","js")
+loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/post_music5.min.js","js")
