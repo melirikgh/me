f58469a3766ff45430fc6a1fbedeba8d18c5544d
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-08-31 19:29:20 +0700

    Update and rename gen0.js to gen1.js

--- js/gen1.js
+++ js/gen1.js
@@ -1,8 +1,8 @@
+/* C-ALL */
+var imgattr = 'loading="lazy" class="lazyload p-e_n" oncontextmenu="return false"';
 /* Analytics4 */
 loadyn('https://www.googletagmanager.com/gtag/js?id='+Analytics4ID,'js');
-
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', Analytics4ID);
