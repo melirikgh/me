0b26936e103dd5c1caf06463c51af9fd9fed1da4
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-08-31 23:32:32 +0700

    Update and rename single4.js to single5.js

--- js/single5.js
+++ js/single5.js
@@ -1,7 +1,7 @@
 /* IMG */
-var imgattr = 'loading="lazy" class="lazyload p-e_n" oncontextmenu="return false"';
-$(".id_img").each(function(){$(this).replaceWith('<div class="text-center"><img '+imgattr+' alt="'+$(this).data("a")+'" src="https://'+$(this).data("s")+'" width="'+$(this).data("w")+'" height="'+$(this).data("h")+'"/></div>')});
+$(".id_img").each(function(){$(this).replaceWith('<div class="text-center"><img loading="lazy" class="lazyload p-e_n" alt="'+$(this).data("a")+'" src="https://'+$(this).data("s")+'" width="'+$(this).data("w")+'" height="'+$(this).data("h")+'" oncontextmenu="return false"/></div>')});
 
 $(document).ready(function(){$("#flippy").click(function(){$("#flippanel").slideToggle("normal")})});
+
 /* Double Click */
 $('i[rel="pre"]').replaceWith(function(){return $("<pre><code>"+$(this).html()+"</code></pre>")});for(var pres=document.querySelectorAll("pre,code,kbd,blockquote,td"),i=0;i<pres.length;i++)pres[i].addEventListener("dblclick",function(){var e=getSelection(),t=document.createRange();t.selectNodeContents(this),e.removeAllRanges(),e.addRange(t)},!1);
