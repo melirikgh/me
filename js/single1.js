575df3d0c24980e3ce28e46650299a8b51181bc3
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-08-31 21:03:59 +0700

    Update and rename single0.js to single1.js

--- js/single1.js
+++ js/single1.js
@@ -1,3 +1,6 @@
+/* IMG */
+$(".id_img").each(function(){$(this).replaceWith('<div class="text-center"><img '+imgattr+' alt="'+$(this).data("a")+'" data-src="https://'+$(this).data("s")+'" width="'+$(this).data("w")+'" height="'+$(this).data("h")+'"/></div>')});
+/* */
 $(document).ready(function(){$("#flippy").click(function(){$("#flippanel").slideToggle("normal")})});
-// Double Click
+/* Double Click */
 $('i[rel="pre"]').replaceWith(function(){return $("<pre><code>"+$(this).html()+"</code></pre>")});for(var pres=document.querySelectorAll("pre,code,kbd,blockquote,td"),i=0;i<pres.length;i++)pres[i].addEventListener("dblclick",function(){var e=getSelection(),t=document.createRange();t.selectNodeContents(this),e.removeAllRanges(),e.addRange(t)},!1);
