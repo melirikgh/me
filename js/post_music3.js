f0bc5851ef654d3697eccbbdc8c61ca88ea213a3
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-08-31 19:51:54 +0700

    Update and rename post_music2.js to post_music3.js

--- js/post_music3.js
+++ js/post_music3.js
@@ -2,7 +2,7 @@ $(".id_img").each(function(){$(this).replaceWith('<div class="text-center"><img 
 
 $(".sumberlirik").replaceWith('<div id="msource"><div class="msource_text">Sumber Lirik &#10095;</div><a href="https://www.google.com/url?sa=t&url=https%3A%2F%2Fwww.musixmatch.com" rel="nofollow noopener" target="_blank" title="Musixmatch"><img '+imgattr+' data-src="https://cdn.statically.io/img/hamdaniazzah.github.io/img/svg/musixmatch.svg"/></a></div>');
 
-$(".id_yt").each(function(){$(this).replaceWith('<div class="yt_class"><div class="yt_text">Dengarkan di YouTube<a href="https://youtu.be/'+$(this).data("id")+'" rel="nofollow noopener" target="_blank"><span id="yt_link"/></a></div><div class="yt_box"><iframe'+lazyon+'data-src="https://www.youtube.com/embed/'+$(this).data("id")+'" frameborder="0" allowfullscreen></iframe></div></div><div class="dengarkan"><div class="listen_text">Dengarkan di &#10095;</div></div>')});
+$(".id_yt").each(function(){$(this).replaceWith('<div class="yt_class"><div class="yt_text">Dengarkan di YouTube<a href="https://youtu.be/'+$(this).data("id")+'" rel="nofollow noopener" target="_blank"><span id="yt_link"/></a></div><div class="yt_box"><iframe '+lazyon+' data-src="https://www.youtube.com/embed/'+$(this).data("id")+'" frameborder="0" allowfullscreen></iframe></div></div><div class="dengarkan"><div class="listen_text">Dengarkan di &#10095;</div></div>')});
 
 var lbox1 = '<div class="listen_box"><a href="https://';
 var lboxS = '" rel="nofollow noopener" target="_blank"><img '+imgattr+' data-src="https://cdn.statically.io/img/hamdaniazzah.github.io/icon/spotify40.svg"/>Spotify</a></div>';
