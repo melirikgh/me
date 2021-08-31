67cf88a6b52817581542fa5716eee8c7ad676213
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-08-31 21:11:43 +0700

    Update and rename gen4.js to gen5.js

--- gen5.js
+++ gen5.js
@@ -1,2 +1,2 @@
 loadyn("https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js","js")
-loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/gen2.min.js","js")
+loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/gen3.min.js","js")
