70c8113ed10f39af6c839ea04f8688633ec6e99d
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-08-31 16:40:07 +0700

    Create gen0.js

---
+++ js/gen0.js
@@ -0,0 +1,8 @@
+/* Analytics4 */
+loadyn('https://www.googletagmanager.com/gtag/js?id='+Analytics4ID,'js');
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', Analytics4ID);
