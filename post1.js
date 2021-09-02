07f76347180693784a47b365a5b33dea0fbbbb48
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-09-02 14:03:48 +0700

    Update and rename post1.js to post2.js

--- post1.js
+++
@@ -1,2 +0,0 @@
-loadyn("https://cdn.statically.io/gh/melirikgh/me/main/css/post0.min.css","css")
-loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/post01.min.js","js")
---
+++ post2.js
@@ -0,0 +1,2 @@
+loadyn("https://cdn.statically.io/gh/melirikgh/me/main/css/post1.min.css","css")
+loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/post3.min.js","js")
