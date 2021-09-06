0cd89a7afe0f73127e85b16a94c72119e5222aa5
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-09-06 15:31:58 +0700

    Update and rename single7.js to single8.js

--- js/single8.js
+++ js/single8.js
@@ -2,7 +2,5 @@
 var bpalt = document.querySelector('title').innerText;
 $(".bpimg").each(function(){$(this).replaceWith('<div class="text-center"><img loading="lazy" class="lazyload p-e_n" alt="'+bpalt+'" data-src="https://1.bp.blogspot.com/'+$(this).data("s")+'/s0-rw/'+bpalt+'.'+$(this).data("f")+'" width="'+$(this).data("w")+'" height="'+$(this).data("h")+'" oncontextmenu="return false"/></div>')});
 
-$(document).ready(function(){$("#flippy").click(function(){$("#flippanel").slideToggle("normal")})});
-
 /* Double Click */
 $('i[rel="pre"]').replaceWith(function(){return $("<pre><code>"+$(this).html()+"</code></pre>")});for(var pres=document.querySelectorAll("pre,code,kbd,blockquote,td"),i=0;i<pres.length;i++)pres[i].addEventListener("dblclick",function(){var e=getSelection(),t=document.createRange();t.selectNodeContents(this),e.removeAllRanges(),e.addRange(t)},!1);
