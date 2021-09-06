c5b073c51f0052abbf3384234a1191f0459c9c6c
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-09-06 14:57:23 +0700

    Update and rename single7.js to single8.js

--- single8.js
+++ single8.js
@@ -1,3 +1,3 @@
-loadyn("https://cdn.statically.io/gh/melirikgh/me/main/css/single1.min.css","js")
+loadyn("https://cdn.statically.io/gh/melirikgh/me/main/css/single1.min.css","css")
 loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/single7.min.js","js")
 loadyn("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js","js")
