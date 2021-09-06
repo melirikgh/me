86b55b1440db4a3f6954d2743ba5f95b1e2ee284
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-09-06 15:38:12 +0700

    Update and rename single9.js to single10.js

---
+++ single10.js
@@ -0,0 +1,4 @@
+loadyn("https://cdn.statically.io/gh/melirikgh/me/main/css/single2.min.css","css")
+loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/single8.min.js","js")
+loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/highlight1.min.js","js")
+
--- single9.js
+++
@@ -1,3 +0,0 @@
-loadyn("https://cdn.statically.io/gh/melirikgh/me/main/css/single1.min.css","css")
-loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/single7.min.js","js")
-loadyn("https://cdn.jsdelivr.net/gh/Arlina-Design/frame@master/highlightr.js","js")
