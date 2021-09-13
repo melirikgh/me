eca54c7d7ae9bcd99d18e138c1b374aa9889ce9a
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-09-13 22:04:49 +0700

    Update and rename post_music8.js to post_music9.js

--- post_music9.js
+++ post_music9.js
@@ -1,2 +1,2 @@
 loadyn("https://cdn.statically.io/gh/melirikgh/me/main/css/post_music1.min.css","css")
-loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/post_music7.min.js","js")
+loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/post_music8.min.js","js")
