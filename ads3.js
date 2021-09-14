e2e48cfc83df18d81cfce30c9c3ded60b3e8399f
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-09-14 14:43:35 +0700

    Update and rename ads3.js to ads4.js

--- ads3.js
+++
@@ -1,2 +0,0 @@
-/* MGID */
-loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/mgid2.min.js","js")
---
+++ ads4.js
@@ -0,0 +1,2 @@
+/* MGID */
+loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/mgid3.min.js","js")
