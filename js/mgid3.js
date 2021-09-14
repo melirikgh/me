124decf78e35bad97ce4774fca88f86f28d33345
melirikgh <86995335+melirikgh@users.noreply.github.com>
2021-09-14 14:43:07 +0700

    Update and rename mgid2.js to mgid3.js

---
+++ js/mgid3.js
@@ -0,0 +1,3 @@
+/* MGID 1 */
+$("#mgid1").each(function(){$(this).replaceWith('<div id="M747553ScriptRootC1199095"></div><style type='text/css'>.mgheader{display:none!important}</style>')});
+loadyn("https://jsc.mgid.com/m/e/melirik.me.1199095.js","js");
