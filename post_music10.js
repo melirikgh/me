68acb3cda91019e31fe422109d98334fd0ae150f
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-02-16 06:03:40 +0700

    Update and rename post_music9.js to post_music10.js

--- post_music10.js
+++ post_music10.js
@@ -1,2 +1,2 @@
 loadyn("https://cdn.statically.io/gh/melirikgh/me/main/css/post_music1.min.css","css")
-loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/post_music8.min.js","js")
+loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/post_music9.min.js","js")
