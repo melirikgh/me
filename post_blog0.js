36da4d0bd7570c65816d54febd8d72e5dfb90711
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-02-16 06:06:20 +0700

    Update and rename post_blog0.js to post_blog1.js

--- post_blog0.js
+++
@@ -1,2 +0,0 @@
-loadyn("https://cdn.statically.io/gh/melirikgh/me/main/css/post_blog0.min.css","css")
-loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/post_blog0.min.js","js")
---
+++ post_blog1.js
@@ -0,0 +1,2 @@
+loadyn("https://cdn.statically.io/gh/melirikgh/me/main/css/post_blog0.min.css","css")
+/*loadyn("https://cdn.statically.io/gh/melirikgh/me/main/js/post_blog0.min.js","js")*/
